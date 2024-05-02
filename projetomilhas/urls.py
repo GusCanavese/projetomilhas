9dbbc8524247bb1d6e20401a56a962618435cef1
Gustavo <bicalhogustavo866@gmail.com>
2024-05-02 10:14:51 -0300

    first

--- projetomilhas/urls.py
+++ projetomilhas/urls.py
@@ -1,7 +1,7 @@
 from django.contrib import admin
 from django.urls import path
 from core import views
-
+ 
 urlpatterns = [
     path('capitania/', admin.site.urls), #admin page
     path('capitania/', views.capitania, name='capitania'), #admin page
