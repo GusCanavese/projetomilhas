745f9f16a04384bb0f2184dd213daed8f243de50
Gustavo <bicalhogustavo866@gmail.com>
2024-05-15 08:33:43 -0300

    finalizando o projeto base

--- core/models.py
+++ core/models.py
@@ -2,17 +2,18 @@ from django.db import models
 
 
 class Membro(models.Model):
-    nome      = models.CharField(max_length=150,  default='null')
-    data      = models.DateField()
-    curso     = models.CharField(max_length=150,  default='null')
-    matricula = models.CharField(max_length=12,   default='null')
-    email     = models.EmailField(max_length=150, default='null')
-    entrada   = models.DateField()
-    saida     = models.DateField()
-    pontos    = models.CharField(max_length=2,    default='null')
-    cargo     = models.CharField(max_length=150,  default='null')
-    info      = models.CharField(max_length=300,  default='null')
-    foto      = models.ImageField(upload_to="foto", default='foto')
+    nome        = models.CharField(max_length=150,    default='null')
+    data        = models.DateField()  
+    curso       = models.CharField(max_length=150,    default='null')
+    matricula   = models.CharField(max_length=12,     default='null')
+    email       = models.EmailField(max_length=150,   default='null')
+    entrada     = models.DateField()  
+    saida       = models.DateField()  
+    pontos      = models.CharField(max_length=2,      default='null')
+    cargo       = models.CharField(max_length=150,    default='null')
+    info        = models.CharField(max_length=300,    default='null')
+    foto        = models.ImageField(upload_to="foto", default='foto')
+    certificado = models.CharField(max_length=3,      default='null', null=True, blank=False)
 
 
     def __str__(self) -> str:
--- core/views.py
+++ core/views.py
@@ -6,27 +6,27 @@ from .models import Membro
 
 # logica que pega os dados do formulario 
 def home(request):
-    nome      = request.POST.get('nome',      None)
-    data      = request.POST.get('data',      None)
-    curso     = request.POST.get('curso',     None)
-    matricula = request.POST.get('matricula', None)
-    email     = request.POST.get('email',     None)
-    entrada   = request.POST.get('entrada',   None)
-    saida     = request.POST.get('saida',     None)
-    pontos    = request.POST.get('pontos',    None)
-    cargo     = request.POST.get('cargo',     None)
-    info      = request.POST.get('geral',     None)
-    foto      = request.POST.get('foto',      None)
-
+    nome        = request.POST.get('nome',        None)
+    data        = request.POST.get('data',        None)
+    curso       = request.POST.get('curso',       None)
+    matricula   = request.POST.get('matricula',   None)
+    email       = request.POST.get('email',       None)
+    entrada     = request.POST.get('entrada',     None)
+    saida       = request.POST.get('saida',       None)
+    pontos      = request.POST.get('pontos',      None)
+    cargo       = request.POST.get('cargo',       None)
+    info        = request.POST.get('geral',       None)
+    foto        = request.POST.get('foto',        None)
+    certificado = request.POST.get('certificado', None)
+    
     
     # pega os valores das variáveis e cadastra no banco de dados
     if request.method == "POST":
-        Membro.objects.create(nome=nome, data=data, curso=curso, matricula=matricula, email=email, entrada=entrada, saida=saida, pontos=pontos, cargo=cargo, info=info, foto=foto)
+        Membro.objects.create(nome=nome, data=data, curso=curso, matricula=matricula, email=email, entrada=entrada, saida=saida, pontos=pontos, cargo=cargo, info=info, foto=foto, certificado=certificado)
         return render(request, 'home.html')
     else:
         return render(request, 'home.html')
-
-
+    
 
 # botao que leva para area administrativa
 def capitania(request):
